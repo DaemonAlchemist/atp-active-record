a0b870865ee52430f3410d791d2705d7aee8fcf5
Andrea <andrea@wittrock.us>
2017-09-14 00:59:33 -0500

    Rename for and optional validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -8,33 +8,33 @@ export default {
     validCollectionFilters: filters => validate(
         (resolve, reject) => {
             validator()
-                .for("columns")
-                    .optional(filters.columns, (v, columns) => v
+                .check("columns")
+                    .isOptional(filters.columns, (v, columns) => v
                         .matches(
                             columns,
                             /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/,
                             "Columns must be a comma-separated list of column names"
                         )
                     )
-                .for("perPage")
-                    .optional(filters.perPage, (v, perPage) => v
+                .check("perPage")
+                    .isOptional(filters.perPage, (v, perPage) => v
                         .isInteger(perPage, "Per page")
                         .greaterThan(perPage, "Per page", 0)
                     )
-                .for("offset")
-                    .optional(filters.offset, (v, offset) => v
+                .check("offset")
+                    .isOptional(filters.offset, (v, offset) => v
                         .isInteger(offset, "Offset")
                         .greaterThan(offset, "Offset", 0)
                     )
-                .for("sort")
-                    .optional(filters.sort, (v, sort) => v
+                .check("sort")
+                    .isOptional(filters.sort, (v, sort) => v
                         .matches(
                             sort,
                             /^[a-zA-Z0-9_]*\\s(ASC|DESC|asc|desc)(,[a-zA-Z0-9_]*\\s(ASC|DESC|asc|desc))*$/,
                             "Sort must be a comma-delimited list of 'column direction' pairs"
                         )
                     )
-                .for("final").if(["columns", "perPage", "offset", "sort"])
+                .check("final").if(["columns", "perPage", "offset", "sort"])
                 .then(resolve, errors => reject(errors));
         },
         "Invalid collection filters",
